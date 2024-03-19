7cf7c1699047b0bb9b82977c0ec023a4783b5d27
Muaj00 <muaj15-14389@diu.edu.bd>
2024-03-19 11:34:46 +0600

    error handle and condition set up

--- js/search.js
+++ js/search.js
@@ -5,15 +5,32 @@ const searchPhone = () => {
 
     searchField.value = '';
 
+    if(searchText == '')
+    {
+        const noSearchResult = document.getElementById('no-search-result');
+        // noSearchResult.innerHTML = '';
+        const div = document.createElement('div');
+        div.classList.add('no-search');
+        div.innerHTML = `
+            <p class = "text-primary fs-1 mt-5 text-center"> Please type in the search box and reload the site </p>
+        `;
+        noSearchResult.appendChild(div); 
+          
+    }
+
+   else {
+    
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
 
     fetch(url)
         .then(res => res.json())
         .then(data => displaySearchResult(data.data));
+   }
 }
 
 const displaySearchResult = data => {
     const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
     data.forEach(phones => {
         // console.log(phones)
 
@@ -34,7 +51,7 @@ const displaySearchResult = data => {
 }
 
 const loadPhoneDetail = phoneId => {
-    console.log(phoneId);
+    // console.log(phoneId);
     const url= `https://openapi.programming-hero.com/api/phone/${phoneId}`;
     fetch(url)
     .then(res => res.json())
@@ -43,5 +60,48 @@ const loadPhoneDetail = phoneId => {
 
 const displayPhoneDetail = phone => {
     console.log(phone);
+    const PhoneDetails = document.getElementById('phone-details');
+    PhoneDetails.textContent = '';
+    // const releaseDate = phone.releaseDate;
+    // console.log(releaseDate);
+
+    if(phone.releaseDate)
+    {
+        const div = document.createElement('div')
+    div.classList.add('card');
+    div.innerHTML = `
+        <div class="w-50 mx-auto"> 
+            <img src="${phone.image}" class="card-img-top " alt="...">
+            <small class="ms-3">${phone.releaseDate}</small>
+        </div>
+        <div class="card-body mx-auto">
+            <h5 class="card-title">${phone.name}</h5>
+            <p class="card-text fw-bold">Brand name: ${phone.brand}</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    `;
+
+        PhoneDetails.appendChild(div);
+    }
+
+    else 
+    {
+        const div = document.createElement('div')
+    div.classList.add('card');
+    div.innerHTML = `
+        <div class="w-50 mx-auto">
+             <img src="${phone.image}" class="card-img-top " alt="..."> 
+             <small> No Release Date Found </small>
+        </div>
+        <div class="card-body mx-auto">
+            <h5 class="card-title">${phone.name}</h5>
+            <p class="card-text fw-bold">Brand name: ${phone.brand}</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    `;
+
+    PhoneDetails.appendChild(div);
+    }
+    
 }
     
